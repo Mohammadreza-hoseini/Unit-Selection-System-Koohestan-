eb82de5d8d7bf602130d4064816c0a9adf97c166
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-03-26 18:45:32 +0330

    add migrations

--- course/models.py
+++ course/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 import uuid
 
-from accounts.models import Professor, Student
-from faculty.models import Faculty
-from term.models import Term
-
 
 class ChooseCourseType(models.IntegerChoices):
     basic = 1, "basic"
@@ -15,10 +11,11 @@ class ChooseCourseType(models.IntegerChoices):
 class Subject(models.Model):
     id = models.CharField(default=uuid.uuid4, editable=False, primary_key=True)
     name = models.CharField(max_length=256)
-    provider_faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='subject_faculty',
+    provider_faculty = models.ForeignKey("faculty.Faculty", on_delete=models.CASCADE,
+                                         related_name='subject_faculty',
                                          verbose_name='دانشکده ارائه دهنده')
-    prerequisite = models.ManyToManyField('self', verbose_name='دروس پیش نیاز')
-    corequisite = models.ManyToManyField('self', verbose_name='دروس همنیاز')
+    prerequisite = models.ManyToManyField('self', verbose_name='دروس پیش نیاز', related_name='subject_prerequisite')
+    corequisite = models.ManyToManyField('self', verbose_name='دروس همنیاز', related_name='subject_corequisite')
     number_of_course = models.PositiveIntegerField(verbose_name='تعداد واحد درس')
     course_type = models.PositiveSmallIntegerField(
         default=ChooseCourseType.basic, choices=ChooseCourseType.choices, verbose_name="نوع درس"
@@ -48,10 +45,10 @@ class Course(models.Model):
         default=ChooseDayOfWeek.saturday, choices=ChooseDayOfWeek.choices, verbose_name="انتخاب روز"
     )
     time = models.TimeField(verbose_name='زمان کلاس')
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='course_professor',
+    professor = models.ForeignKey("accounts.Professor", on_delete=models.CASCADE, related_name='course_professor',
                                   verbose_name='استاد')
     capacity = models.PositiveIntegerField(verbose_name='ظرفیت')
-    term = models.ForeignKey(Term, on_delete=models.CASCADE, related_name='course_term', verbose_name='ترم')
+    term = models.ForeignKey("term.Term", on_delete=models.CASCADE, related_name='course_term', verbose_name='ترم')
     exam_time = models.DateTimeField(verbose_name='تاریخ و زمان امتحان')
     exam_class_id = models.PositiveIntegerField(verbose_name='شماره کلاس امتحان')
 

cbcdb716de413f2e0927fb6cf6211b1efea900f0
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-03-28 11:38:35 +0330

    Revert "make serializers.py"
    This reverts commit fe3a3cba83572362b7ed4c15f256fe41daaa6607.

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,3 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Professor
-from rest_framework import status
-from .serializers import ProfessorSerializer
+from django.shortcuts import render
 
-
-class ProfessorCreateView(APIView):
-    queryset = Professor.objects.all()
-    serializer_class = ProfessorSerializer
-
-    def get(self, request, *args, **kwargs):
-        professors = Professor.objects.all()
-        serializer = self.serializer_class(professors, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.

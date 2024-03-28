993e8eb80a73e1fcc0ce1283849bcf53f83ca424
arman <shakerian.arman@yahoo.com>
2024-03-28 21:18:37 +0330

    add EA view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Student
-from .serializers import StudentSerializer
+from .models import Student, EducationalAssistant
+from .serializers import StudentSerializer, EducationalAssistantSerializer
 
 
 class StudentCreate(APIView):
@@ -28,3 +28,32 @@ class StudentCreate(APIView):
         # Serialize the queryset into JSON
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
+
+
+class EducationalAssistantView(APIView):
+    def post(self, request):
+        """
+        Create a new EA -> Select from Professors
+        """
+
+        # Check if the user is IT-admin #TODO
+
+        # filtering based on project documentation #TODO
+
+        # request.data is only the national_code #BUG
+        serializer = EducationalAssistantSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """
+        Return list of all EAs
+        """
+        EAs = EducationalAssistant.objects.all()
+        serializer = EducationalAssistantSerializer(EAs, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk): ...
+    def delete(self, request, pk): ...

cca9f3404a106104ffb9a92acbaf36fd38a92378
arman <shakerian.arman@yahoo.com>
2024-04-01 14:31:42 +0330

    add EA_ModelFilter

--- accounts/FilterSet.py
+++ accounts/FilterSet.py
@@ -1,5 +1,6 @@
+from os import name
 import django_filters
-from .models import Student
+from .models import Student, EducationalAssistant
 
 
 class StudentModelFilter(django_filters.FilterSet):
@@ -16,3 +17,27 @@ class StudentModelFilter(django_filters.FilterSet):
             'entry_year': ['exact'],
             'military_service_status': ['exact'],
         }
+
+
+class EA_ModelFilter(django_filters.FilterSet):
+    
+    firstname = django_filters.CharFilter(field_name='assistant__firstname')
+    lastname = django_filters.CharFilter(field_name='assistant__lastname')
+    pr_number = django_filters.UUIDFilter(field_name='assistant__professor_number')
+    national_code = django_filters.NumberFilter(field_name='assistant__national_code')
+    
+    faculty = django_filters.UUIDFilter(field_name='faculty__id')
+    major = django_filters.UUIDFilter(field_name='assistant__major__id')
+    
+    
+    class Meta:
+        model = EducationalAssistant
+        
+        # see project document TODO
+        fields = ['firstname', 
+                  'lastname', 
+                  'pr_number', 
+                  'national_code', 
+                  'faculty', 
+                  'major']
+        

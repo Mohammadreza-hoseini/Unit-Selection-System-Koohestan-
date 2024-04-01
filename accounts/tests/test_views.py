0cb2c426a1cc765760376b054bad0d8afdcf1a17
arman <shakerian.arman@yahoo.com>
2024-04-01 14:47:28 +0330

    minor changes in pk due to EA model change

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -6,40 +6,39 @@ from rest_framework.test import APITestCase
 
 class TestEAviews(APITestCase):
 
-    # TODO
+    # create new dumpdata in data.json #TODO
     fixtures = ["data.json"]
     
 
     def test_list_EA(self):
-        response = self.client.get(reverse("accounts:educationalAssistant"))
+        response = self.client.get(reverse("accounts:assistants_get_all"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_EA(self):
         EA_sample = {
-            "educational_assistant": "6a5c1aca-5088-45c4-abbe-8bbdcd294bfd",
-            "assistant": "0d4c318e-f6f3-4a25-994b-9d141038f5ec",
-            "faculty": "66971b0d-c482-46cd-ab6f-73a45322685d",
+            "assistant": "99c9a225-a9a8-448d-903d-119c61f619ac",
+            "faculty": "0cba684b-58c9-4727-8a17-c51efbd847be"
         }
         response = self.client.post(reverse("accounts:educationalAssistant"), EA_sample)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_EA_by_wrongPK_should_return_HTTP400(self):
-        pk = "b3a2b78f-6742-4f01-a2c9-63cf1df1d956"
+        pk = "0cba684b-58c9-4727-8a17-c51efbd847be"
         response = self.client.get(reverse("accounts:EA_withPK", args=[pk]))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_EA_by_correctPK_should_return_HTTP200(self):
-        pk = "425d2328-84fd-4bd3-be0a-12bd81999443"
+        pk = "ed1c658b-13ed-4f62-87e7-dd15b5bc040f"
         response = self.client.get(reverse("accounts:EA_withPK", args=[pk]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_EA_by_wrongPK_return_HTTP400(self):
-        pk = "b3a2b78f-6742-4f01-a2c9-63cf1df1d956"
+        pk = "0cba684b-58c9-4727-8a17-c51efbd847be"
         response = self.client.delete(reverse("accounts:EA_withPK", args=[pk]))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_delete_EA_by_correctPK_return_HTTP200(self):
-        pk = "425d2328-84fd-4bd3-be0a-12bd81999443"
+        pk = "ed1c658b-13ed-4f62-87e7-dd15b5bc040f"
         response = self.client.delete(reverse("accounts:EA_withPK", args=[pk]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)

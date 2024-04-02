52a62d809b338730a58db130e0b3c1b346645b8d
arman <shakerian.arman@yahoo.com>
2024-04-02 14:07:06 +0330

    change url of Term creation

--- term/urls.py
+++ term/urls.py
@@ -3,7 +3,7 @@ from .views import TermView, GetAll_terms, TermWithPK
 
 app_name = 'term'
 urlpatterns = [
-    path("term/", TermView.as_view(), name="create_term"),
+    path("create_term/", TermView.as_view(), name="create_term"),
     path("term_get_all/", GetAll_terms.as_view(), name="term_get_all"),
-    path("term/<uuid:pk>", TermWithPK.as_view(), name='Term_withPK'),
+    path("termPK/<uuid:pk>", TermWithPK.as_view(), name='Term_withPK'),
 ]

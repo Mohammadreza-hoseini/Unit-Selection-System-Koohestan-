7f6b1fa393692035a0252784d86eface80b9782e
arman <shakerian.arman@yahoo.com>
2024-04-02 12:23:53 +0330

    Term CRUD

--- term/views.py
+++ term/views.py
@@ -1,3 +1,75 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from django_filters import rest_framework as filters
+
+from .FilterSet import TermModelFilter
+from .models import Term
+from .serializers import TermSerializer, TermGetDataSerializer
+
+
+class TermView(APIView):
+    """
+    Create new Term
+    """
+    
+    def post(self, request):
+        serializer = TermSerializer(request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class GetAll_terms(ListAPIView):
+    """
+        Return list of all terms
+    """
+    
+    serializer_class = TermSerializer
+    queryset = Term.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = TermModelFilter
+
+class TermWithPK(APIView):
+    def get(self, request, pk):
+        """
+        Return a term
+        """
+        try:
+            term_obj = Term.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response("This Term doesn't exist", status=status.HTTP_400_BAD_REQUEST)
+        serializer = TermGetDataSerializer(term_obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def put(self, request, pk):
+        """
+        Update a term
+        """
+        
+        try:
+            term_obj = Term.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response("This Term doesn't exist", status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = TermSerializer(request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.update(instance=term_obj, validated_data=serializer)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        """
+        Delete a term
+        """
+        
+        try:
+            term_obj = Term.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return Response("This Term doesn't exist", status=status.HTTP_400_BAD_REQUEST)
+        term_obj.delete()
+        return Response("Successfully deleted", status=status.HTTP_200_OK)

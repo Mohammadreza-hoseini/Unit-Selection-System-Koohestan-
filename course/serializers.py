f1bec848535ed2f9e6468778a00eb2758d0efaf1
arman <shakerian.arman@yahoo.com>
2024-04-03 00:16:00 +0330

    add Course serializers

--- course/serializers.py
+++ course/serializers.py
@@ -2,57 +2,93 @@ from wsgiref.validate import validator
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+
+from django.utils import timezone
+
 from django.db import transaction
 
-from .models import Term
+from .models import Course, Subject
 
-from accounts.models import Student, Professor
+from term.models import Term
+from accounts.models import Professor
 
 
-def validate_times(attrs):
-    true_order = (attrs['start_selection_time'] < attrs['end_selection_time']
-            < attrs['class_start_time'] < attrs['doped_added_start_time']
-            < attrs['doped_added_end_time'] < attrs['emergency_removal_end_time']
-            < attrs['class_end_time'] < attrs['exam_start_time']
-            < attrs['term_end_time'])
-    if not true_order:
-        raise ValidationError("""Order of times should be: \
-                            start_selection_time <  end_selection_time < class_start_time < doped_added_start_time < doped_added_end_time < emergency_removal_end_time < class_end_time < exam_start_time < term_end_time)""")
 
-class TermSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    start_selection_time = serializers.DateTimeField()
-    end_selection_time = serializers.DateTimeField()
-    class_start_time = serializers.DateTimeField()
-    class_end_time = serializers.DateTimeField()
-    doped_added_start_time = serializers.DateTimeField()
-    doped_added_end_time = serializers.DateTimeField()
-    emergency_removal_end_time = serializers.DateTimeField()
-    exam_start_time = serializers.DateTimeField()
-    term_end_time = serializers.DateTimeField()
+
+# validations:
+    # 1) 'professor.faculty' matches 'subject.provider_faculty' #DONE
+    # 2) class with 'class_id' should be empty in that 'day' and 'time' #DONE
+    # 3) 'exam_time' should be between 'term.exam_start_time' and 'term.exam_end_time' #DONE 
+    # 4) class with 'exam_class_id' should be empty in that 'exam_time'
+    # 5) optional: classModel.capacity >= course.capacity
+
+def validate_time(attrs):
+    # Validation 1
+            # Check if the doped_added_end_time has passed or not
+    term_doped_added_end_time = attrs['term'].doped_added_end_time
+    current_datetime = timezone.now()
+    if current_datetime >= term_doped_added_end_time:
+        raise ValidationError("term_doped_added_end_time has passed")
+
+
+def validate_professorFaculty_subject(attrs):
+    # Validation 2
+                # Check if professor.faculty matches subject.provider_faculty
+    prof_faculty = attrs['professor'].faculty.id
+    subject_provider_faculty = attrs['subject'].provider_faculty.id
+    if prof_faculty != subject_provider_faculty:
+        raise ValidationError("Professor_faculty and subject_provider_faculty don't match")
+
+def validate_exam_time(attrs):
+    exam_time = attrs['exam_time']
+    exams_start_time = attrs['term'].exam_start_time
+    exams_end_time = attrs['term'].exam_end_time
+
+    if not exams_start_time <= exam_time <= exams_end_time:
+        raise ValidationError("Times should be: term.exams_start_time <= exam_time <= term.exams_end_time")
+
+class CourseSerializer(serializers.Serializer):
+    subject = serializers.PrimaryKeyRelatedField(queryset=Subject.objects.all())
+    term = serializers.PrimaryKeyRelatedField(queryset=Term.objects.all())
+    professor = serializers.PrimaryKeyRelatedField(queryset=Professor.objects.all())
+    # class_id -> new table for class? #TODO
+    
+    day = serializers.IntegerField()
+    time = serializers.TimeField()
+    capacity = serializers.IntegerField()
+    exam_time = serializers.DateTimeField()
+    exam_class_id = serializers.IntegerField()
     
     
+    
+    #TODO
     def validate(self, attrs):
-        """
-        Object-level validation
-        """
-        validate_times(attrs)
+        
+        validate_time(attrs)
+        
+        validate_professorFaculty_subject(attrs)
+        
+        validate_exam_time(attrs)
+        
+        
         return attrs
     
+    #TODO
     @transaction.atomic
     def create(self, validated_data):
+        # "only IT_Manager & related_EA can create a course -> in view?"
+        
         return Term.objects.create(**validated_data)
     
+    #TODO
     @transaction.atomic
     def update(self, instance, validated_data):
-        instance.name = validated_data.data.get('name', instance.name)
-        if Term.objects.exclude(id=instance.id).filter(name=instance.name).exists():
-            raise ValidationError("There is already a term with this name")
-        instance.save()
+        ...
+        
         return instance
     
-class TermGetDataSerializer(serializers.ModelSerializer):
+class CourseGetDataSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = Term
+        model = Course
         fields = "__all__"

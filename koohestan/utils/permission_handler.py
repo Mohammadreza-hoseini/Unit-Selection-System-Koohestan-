2a4bef78b29d24a0212ab8b2465abd5ba6f771e0
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-04-05 12:30:52 +0330

    add selfPermission for EA and student and professor

--- koohestan/utils/permission_handler.py
+++ koohestan/utils/permission_handler.py
@@ -23,3 +23,21 @@ class ProfessorPermission(BasePermission):
     def has_permission(self, request, view):
         if request.user.role == 2:
             return request.user and request.user.is_authenticated
+
+
+class StudentSelfPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role == 1 and request.user.id == str(view.kwargs['pk']):
+            return request.user and request.user.is_authenticated
+
+
+class EASelfPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role == 4 and request.user.id == str(view.kwargs['pk']):
+            return request.user and request.user.is_authenticated
+
+
+class ProfessorSelfPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role == 2 and request.user.id == str(view.kwargs['pk']):
+            return request.user and request.user.is_authenticated

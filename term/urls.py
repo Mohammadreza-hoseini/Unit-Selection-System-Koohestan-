c92ce738309f999ac5125fa196a781943433bc61
arman <shakerian.arman@yahoo.com>
2024-04-05 14:07:53 +0330

    minor change

--- term/urls.py
+++ term/urls.py
@@ -5,5 +5,5 @@ app_name = 'term'
 urlpatterns = [
     path("create_term/", TermView.as_view(), name="create_term"),
     path("term_get_all/", GetAll_terms.as_view(), name="term_get_all"),
-    path("termPK/<uuid:pk>", TermWithPK.as_view(), name='Term_withPK'),
+    path("termPK/<uuid:pk>", TermWithPK.as_view(), name='Term_withPK'),   
 ]

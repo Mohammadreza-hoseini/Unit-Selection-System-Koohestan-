b94685840d2a90a881832ed0df21b952aa873095
arman <shakerian.arman@yahoo.com>
2024-04-06 23:18:34 +0330

    add modular Tr, Unit_selection paths

--- term/urls.py
+++ term/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 app_name = 'term'
 
 urlpatterns = [
-    path('', include('term.Tr.tr_urls')), 
+    path('', include('term.Tr.tr_urls')),
+    path('', include('term.Unit_selection.unit_urls')),
 ]

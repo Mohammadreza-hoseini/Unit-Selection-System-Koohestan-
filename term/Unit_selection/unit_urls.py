02aed17e3d028ddf52fe421a094fe2e42d0640b9
arman <shakerian.arman@yahoo.com>
2024-04-07 20:54:23 +0330

    add create method of UR_serializer

--- term/Unit_selection/unit_urls.py
+++ term/Unit_selection/unit_urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from term.Unit_selection.unit_views import UR_CreateView, UR_GetView
+
 urlpatterns = [
+    path('student/<uuid:st_pk>/course_selection/create/', UR_CreateView.as_view(), name='create_courseSelectionForm'),
+    path('student/<uuid:st_pk>/course_selection/', UR_GetView.as_view(), name='get_courseSelectionDetails'),
 ]

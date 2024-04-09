4ab7ab937d7128f585a7be6c9681851d267e5792
arman <shakerian.arman@yahoo.com>
2024-04-09 12:32:09 +0330

    add term_obj to URFormSerializer due to UR_model change

--- term/Unit_selection/unit_serializers.py
+++ term/Unit_selection/unit_serializers.py
@@ -5,13 +5,14 @@ from rest_framework.exceptions import ValidationError
 
 from course.models import Course
 from course.serializers import CourseGetDataSerializer
-from term.models import UnitRegisterRequest
+from term.models import Term, UnitRegisterRequest
 
 from .unit_validations import validate_passed_course, validate_student_add_unit_average, \
     validate_exam_and_class_time_interference, validate_courses_related_to_the_field, validate_prerequisite_subject_passed, validate_course_capacity
 
 
 class URFormSerializer(serializers.Serializer):
+    term = serializers.PrimaryKeyRelatedField(queryset=Term.objects.all())
     course = serializers.ListField()
 
     def validate(self, attrs):
@@ -33,11 +34,18 @@ class URFormSerializer(serializers.Serializer):
     def create(self, validated_data):
 
         courses = validated_data['course']
+        term_id = validated_data['term']
 
         # retrieve additional_data in self.context
         student_obj = self.context.get('student_obj')
-
-        UR_form_obj = UnitRegisterRequest(student=student_obj)
+        
+        #check if term.id = course.term.id #TODO 
+        try:
+            term_obj = Term.objects.get(id=term_id)
+        except:
+            raise serializers.ValidationError('This term does not exist')    
+                
+        UR_form_obj = UnitRegisterRequest(student=student_obj, term=term_obj)
 
         for course in courses:
             if not Course.objects.filter(id=course).exists():

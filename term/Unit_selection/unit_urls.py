0da3ad3a9cf082d562f57b40556f2c8d69071dbb
arman <shakerian.arman@yahoo.com>
2024-04-09 20:07:59 +0330

    change path names |  add URGetStPk | change URGetView path

--- term/Unit_selection/unit_urls.py
+++ term/Unit_selection/unit_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from term.Unit_selection.unit_views import URCreateView, URGetView
+from term.Unit_selection.unit_views import URCreateView, URGetView, URGetStPk
 
 urlpatterns = [
-    path('student/<uuid:st_pk>/course_selection/create/', URCreateView.as_view(), name='create_courseSelectionForm'),
-    path('student/<uuid:st_pk>/course_selection/', URGetView.as_view(), name='get_courseSelectionDetails'),
+    path('student/<uuid:st_pk>/course_selection/create/', URCreateView.as_view(), name='create_UR'),
+    path('student/course_selection/', URGetView.as_view(), name='get_allURDetails'),
+    path('student/<uuid:st_pk>/course_selection/', URGetStPk.as_view(), name='get_StURDetails'),
 ]

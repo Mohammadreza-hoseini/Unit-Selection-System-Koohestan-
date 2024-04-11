ef198ccc5a023c9d81517efd5ebdeb951c1eefcb
arman <shakerian.arman@yahoo.com>
2024-04-11 12:17:18 +0330

    add ScoreTableView path

--- course/appeal_requests/ApReq_urls.py
+++ course/appeal_requests/ApReq_urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from course.appeal_requests.ApReq_views import ScoreTableView
+
 urlpatterns = [
+    
+    # just for test #TODO
+    path('prof/score/', ScoreTableView.as_view(), name='add_score'),
 ]

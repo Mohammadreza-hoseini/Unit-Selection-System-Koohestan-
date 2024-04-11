7ba12a8bf3855728ca6270f7e9f3a439b8d03bdc
arman <shakerian.arman@yahoo.com>
2024-04-11 12:46:38 +0330

    debug ScoreTableView post method

--- course/appeal_requests/ApReq_views.py
+++ course/appeal_requests/ApReq_views.py
@@ -9,6 +9,7 @@ from django_filters import rest_framework as filters
 
 from accounts.models import Professor, Student
 from course.appeal_requests.ApReq_serializers import ScoreTableSerializer
+from course.models import Course
 from faculty.serializers import UniversityGetDataSerializer
 
 from term.Unit_selection.unit_serializers import (
@@ -32,13 +33,23 @@ class ScoreTableView(APIView):
     def post(self, request, pr_pk, course_pk):
         
         try:
-            get_pr = Professor.objects.get(id=pr)
-        except print(0):
-            pass
+            get_prof = Professor.objects.get(id=pr_pk)
+        except ObjectDoesNotExist:
+            return Response(
+                "This Professor does not exist", status=status.HTTP_404_NOT_FOUND
+            )
         
-        additional_data = {}
-        print(request.data)
-        serializer = ScoreTableSerializer(data=request.data)
+        try:
+            get_course = Course.objects.get(id=course_pk)
+        except ObjectDoesNotExist:
+            return Response(
+                "This Course does not exist", status=status.HTTP_404_NOT_FOUND
+            )
+        
+        
+        additional_data = {'prof_obj': get_prof, 'course_obj': get_course}
+        
+        serializer = ScoreTableSerializer(data=request.data, context=additional_data)
         if serializer.is_valid(raise_exception=True):
             print("yesss")
             serializer.save()

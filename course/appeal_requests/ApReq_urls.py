a0f35690f528a8999d833842b40cc9daa6db24a8
ArmanShakerian <shakerian.arman@yahoo.com>
2024-04-12 12:29:26 +0330

    add ApReqView path

--- course/appeal_requests/ApReq_urls.py
+++ course/appeal_requests/ApReq_urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from course.appeal_requests.ApReq_views import ScoreTableView
+from course.appeal_requests.ApReq_views import ScoreTableView, ApReqView
 
 urlpatterns = [
-    
-    # just for test #TODO
     path('prof/score/<uuid:pr_pk>/<uuid:course_pk>', ScoreTableView.as_view(), name='add_score'),
+    path('student/<uuid:st_pk>/appeal_request/<uuid:course_pk>', ApReqView.as_view(), name='apply_req')
 ]

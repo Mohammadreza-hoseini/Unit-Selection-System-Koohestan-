91b4c1b96c6316a6a47984a93485434e9d70b66e
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-04-12 21:29:46 +0330

    emergency removal done

--- term/Emergency_Removal/er_urls.py
+++ term/Emergency_Removal/er_urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .er_views import StudentEmergencyRemoval, AssistantAcceptOrRejectEmergencyRemoval
+from .er_views import StudentEmergencyRemoval, AssistantAcceptOrRejectEmergencyRemoval, \
+    AssistantGetAllAcceptOrRejectEmergencyRemoval
 
 urlpatterns = [
     path('student/<uuid:student_id>/courses/<uuid:course_id>/emergency-remove/', StudentEmergencyRemoval.as_view(),
          name='student_emergency_removal'),
     path('assistant/<uuid:assistant_id>/emergency-remove/<uuid:emergency_removal_id>/',
          AssistantAcceptOrRejectEmergencyRemoval.as_view(),
-         name='assistant_emergency_removal')
+         name='assistant_emergency_removal'),
+    path('assistant/<uuid:assistant_id>/emergency-remove/', AssistantGetAllAcceptOrRejectEmergencyRemoval.as_view(),
+         name='assistant_getAll_emergency_removal_requests'),
+
 ]
